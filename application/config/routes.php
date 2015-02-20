5eeff6ce4e04372c58f7f08737ec902535498c76
waten <waten1992@gmail.com>
2015-02-20 17:03:01 +0800

    读取新闻条目
    利用数据库

--- application/config/routes.php
+++ application/config/routes.php
@@ -37,13 +37,19 @@
 | in the URL cannot be matched to a valid route.
 |
 */
+/*
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
+*/
 /*
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
   */
 
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
 
 
 /* End of file routes.php */
--- application/models/news_model.php
+++ application/models/news_model.php
@@ -1,15 +1,25 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-class News_model extends CI_Model 
-{
 
-  public function __construct()
-  {
-    $this->load->database();
-  }
+class News_model extends CI_Model {
+
+    public function __construct() {
+        $this->load->database();
+    }
+
+    public function get_news($slug = FALSE) {
+        if ($slug === FALSE) {
+            $query = $this->db->get('news');
+            return $query->result_array();
+        }
+
+        $query = $this->db->get_where('news', array('slug' => $slug));
+        return $query->row_array();
+    }
+
 }
