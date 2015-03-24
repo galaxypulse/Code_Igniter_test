95ea5ab6dd42e31c36b7493da34f95b57497069a
waten <waten1992@gmail.com>
2015-03-24 23:51:19 +0800

    增加验证

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -6,6 +6,11 @@ class Form extends CI_Controller {
         $this->load->helper(array('form', 'url'));
 
         $this->load->library('form_validation');
+        
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
 
         if ($this->form_validation->run() == FALSE) {
             $this->load->view('myform');
