920c294b128564520687cb4fe444b95d2bf8740e
waten <waten1992@gmail.com>
2015-04-11 09:00:23 +0800

    修改路由配置
    按照文档修改路由配置实验

--- application/config/routes.php
+++ application/config/routes.php
@@ -47,8 +47,9 @@ $route['404_override'] = '';
   */
 
 
-$route['default_controller'] = 'form/index';
-$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'blog'; //系统默认的
+$route['pages/view'] = 'pages/view/$1';  //
+$route['404_override'] = ''; //报错处理页面
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
