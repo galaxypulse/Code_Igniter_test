ecfb3cb505b3087d1870920d15f8738b51c62bc4
waten <waten1992@gmail.com>
2015-04-11 09:31:26 +0800

    controller 实例

---
+++ application/controllers/blog.php
@@ -0,0 +1,13 @@
+<?php
+
+class Blog extends CI_Controller
+{
+    public function  index()
+    {
+        echo "hello world";
+    }
+      public function comments()
+        {
+                echo 'Look at this!';
+        }
+}
--- application/views/myform.php
+++ application/views/myform.php
@@ -12,7 +12,7 @@
         <input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" />
 
         <h5>Password</h5>
-        <input type="text" name="password" value="<?php echo set_value('password'); ?>" size="50" />
+        <input type="text" name="password" value= "<?php echo set_value('password'); ?>" size="50" />
 
         <h5>Password Confirm</h5>
         <input type="text" name="passconf" value="<?php echo set_value('passconf'); ?>" size="50" />
