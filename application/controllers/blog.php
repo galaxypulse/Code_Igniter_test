8d18e42cda6596c2f79d8283ce77194688b477fb
waten <waten1992@gmail.com>
2015-04-14 14:03:07 +0800

    增加辅助元素
    $this->load->helper('url');

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -1,13 +1,25 @@
 <?php
 
-class Blog extends CI_Controller
-{
-    public function  index()
-    {
-        echo "hello world";
+class Blog extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
     }
-      public function comments()
-        {
-                echo 'Look at this!';
-        }
+
+    public function index() {
+        echo "hello world";
+        //  $this->load->view('blogview');
+        $this->load->helper('url');
+        $data['title'] = "My Real Title";
+        $data['heading'] = "My Real Heading";
+        $data['todo_list'] = array('Clean House', 'Call Mom', 'Run Errands');
+        $this->load->view('templates/header', $data);
+
+        $this->load->view('templates/footer');
+    }
+
+    public function comments() {
+        echo 'Look at this! </br>';
+    }
+
 }
