113bba4e7733439087230ca97209b23970409b80
waten <waten1992@gmail.com>
2015-04-15 23:50:34 +0800

    信息插入DB中

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -2,15 +2,25 @@
 
 class Form extends CI_Controller {
 
-    function index() {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('news_model');
+        $this->load->model('register_model');
         $this->load->helper(array('form', 'url'));
+        $this->load->helper('security');
+    }
+
+    function index() {
+
 
         $this->load->library('form_validation');
-        
-        $this->form_validation->set_rules('username', 'Username', 'required');
-        $this->form_validation->set_rules('password', 'Password', 'required');
-        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
-        $this->form_validation->set_rules('email', 'Email', 'required');
+
+        $this->form_validation->set_rules('user_id', 'user_id', 'trim|required|min_length[5]|max_length[12]|xss_clean');
+        $this->form_validation->set_rules('passwd', 'passwd', 'trim|required|matches[passconf]|md5');
+        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'trim|required');
+        $this->form_validation->set_rules('address', 'address', 'trim|required|valid_email');
+
+        $this->load->library('form_validation');
 
         if ($this->form_validation->run() == FALSE) {
             $this->load->view('myform');
@@ -19,6 +29,15 @@ class Form extends CI_Controller {
         }
     }
 
-}
+    function insert_register() {
+        $post_data = array(//获取数据
+            'user_id' => $this->input->post('user_id'),
+            'passwd' => $this->input->post('passwd'),
+            'address' => $this->input->post('address')
+        );
+        $post_data['passwd'] = do_hash($post_data['passwd']); //默认是SHA1
+        $this->db->insert('users', $post_data);
+        echo 'Insert success!';
+    }
 
-?>
+}
