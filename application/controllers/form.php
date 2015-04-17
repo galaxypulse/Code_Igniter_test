aa27e23f074b89f8f28c8fd59a3720830ea4e6c0
waten <waten1992@gmail.com>
2015-04-18 01:35:17 +0800

    HASH一定要以相同的方式HASH
    否者，DB 的HASH值和表单获取的值不同，搞了一个多小时的HASH值不同的问题，卧槽～～

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -9,29 +9,31 @@ class Form extends CI_Controller {
         $this->load->helper(array('form', 'url', 'html', 'security'));
         $this->load->library('form_validation');
         $this->load->library('image_lib');
+        $this->load->library('encrypt'); //加密函数和session
+        $this->load->library('session');
     }
 
     function register() {
-
         if ($this->form_validation->run('signup') == FALSE) { //通不过验证就提示重新注册
             $this->load->view('myform');
-        } else {
+        } else { //插入到数据库中
             $post_data = array(//获取数据
                 'user_id' => $this->input->post('user_id'),
                 'passwd' => $this->input->post('passwd'),
                 'address' => $this->input->post('address')
             );
-            $post_data['passwd'] = do_hash($post_data['passwd']); //默认是SHA1
+            $post_data['passwd'] = do_hash($this->input->post('passwd'),'md5'); //默认是SHA1,我选择了MD5 
+           // 数据库中的注册和登录验证的要以相同的方式获取，否者hash出来的东西不一样，比如用：$this->input->post('passwd')
             $this->db->insert('users', $post_data);
 
-            $this->load->view('home'); //return home 
+            $this->load->view('home'); //默认返回主页
         }
     }
 
-    public function user_id_check($tel) { //
-        $iphone = preg_match("/^13[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/",$tel); 
+    public function user_id_check($tel) { //检查是否是真正的手机号码
+        $iphone = preg_match("/^13[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/", $tel);
         if (!$iphone) {
-            $this->form_validation->set_message('user_id_check','你的 %s number 不对');
+            $this->form_validation->set_message('user_id_check', '你的 %s number 不对');
             return FALSE;
         } else {
             return TRUE;
@@ -40,9 +42,16 @@ class Form extends CI_Controller {
 
     function login() {
         $this->form_validation->set_rules('user_id', 'user_id', 'trim|required|min_length[10]|max_length[12]|xss_clean');
-        $this->form_validation->set_rules('passwd', 'passwd', 'trim|required|matches[passconf]|md5');
+        $this->form_validation->set_rules('passwd', 'passwd', 'trim|required');
+       
+        $iphone = $this->input->post('user_id');
+        $pwd = $this->input->post('passwd');
+      
+        $pwd = do_hash($pwd, 'md5');
+      
+        $query = $this->db->get_where('users', array('user_id' => $iphone,'passwd' => $pwd));
 
-        if ($this->form_validation->run() == FALSE) {
+        if ($this->form_validation->run() == FALSE  || $query->num_rows() < 1) {
             $this->load->view('pages/login');
         } else {
             $this->load->view('home');
@@ -55,6 +64,7 @@ class Form extends CI_Controller {
 
     function home() {
         $this->load->view('home');
+
     }
 
     function about() {
