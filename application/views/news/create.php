369b6a81cc4d129efd27dbeb66fefdb26d8546b2
waten <waten1992@gmail.com>
2015-04-18 02:05:37 +0800

    去除index.php
    结果没有除去

--- application/views/news/create.php
+++ application/views/news/create.php
@@ -2,14 +2,14 @@
 
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('news/create') ?>
+<?php echo form_open('news/insert') ?>
 
-  <label for="title">Title</label> 
-  <input type="input" name="title" /><br />
+<label for="title">Title</label> 
+<input type="input" name="title" /><br />
 
-  <label for="text">Text</label>
-  <textarea name="text"></textarea><br />
-  
-  <input type="submit" name="submit" value="Create news item" /> 
+<label for="text">Text</label>
+<textarea name="text"></textarea><br />
+
+<input type="submit" name="submit" value="Create news item" /> 
 
 </form>
