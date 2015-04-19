55ae052c8594353f5777a4bcce7d543c1c69974a
waten <waten1992@gmail.com>
2015-04-19 20:26:00 +0800

    myself 方法的页面

---
+++ application/views/log/include_logout.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="utf-8" lang="utf-8">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>My Form</title>
+    </head>
+    <style>
+
+        body {
+            width: 35em;
+            margin: 0 auto;
+            padding: 5px;
+            font-family: Tahoma, Verdana, Arial, sans-serif;
+        }
+        ul
+        {
+            list-style-type:none;
+            margin:0;
+            padding:0;
+            overflow:hidden;
+        }
+        li
+        {
+            float:left;
+        }
+        a:link,a:visited
+        {
+            display:block;
+            width:120px;
+            font-weight:bold;
+            color:#FFFFFF;
+            background-color:#bebebe;
+            text-align:center;
+            padding:4px;
+            text-decoration:none;
+            text-transform:uppercase;
+        }
+        a:hover,a:active
+        {
+            background-color:#cc0000;
+        }
+
+    </style>
+    <body>
+        <ul>
+            <li><a href = "http://localhost/index.php/form/home" class="button" >Home</a></li>
+            <li><a href = "http://localhost/index.php/form/contact">Contact</a></li>
+            <li><a href = "http://localhost/index.php/form/about">About</a></li>
+            <li><a href = "http://localhost/index.php/form/myself">You</a></li>
+            <li><a href = "http://localhost/index.php/form/logout">Logout</a></li>
+
+        </ul>
+        <?PHP
+        echo form_button('name', 'Logout');
+        ?>
+        </form>
+
+    </body>
+</html>
