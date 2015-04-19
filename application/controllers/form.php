cc142d3df6226e4cc1bc05ae371a7a95d90c51fa
waten <waten1992@gmail.com>
2015-04-19 20:25:27 +0800

    增加session 和 logout myself 方法

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -22,8 +22,8 @@ class Form extends CI_Controller {
                 'passwd' => $this->input->post('passwd'),
                 'address' => $this->input->post('address')
             );
-            $post_data['passwd'] = do_hash($this->input->post('passwd'),'md5'); //默认是SHA1,我选择了MD5 
-           // 数据库中的注册和登录验证的要以相同的方式获取，否者hash出来的东西不一样，比如用：$this->input->post('passwd')
+            $post_data['passwd'] = do_hash($this->input->post('passwd'), 'md5'); //默认是SHA1,我选择了MD5 
+            // 数据库中的注册和登录验证的要以相同的方式获取，否者hash出来的东西不一样，比如用：$this->input->post('passwd')
             $this->db->insert('users', $post_data);
 
             $this->load->view('home'); //默认返回主页
@@ -33,7 +33,7 @@ class Form extends CI_Controller {
     public function user_id_check($tel) { //检查是否是真正的手机号码
         $iphone = preg_match("/^13[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/", $tel);
         if (!$iphone) {
-            $this->form_validation->set_message('user_id_check', '你的 %s number 不对');
+            $this->form_validation->set_message('user_id_check', '你的 %s  不对');
             return FALSE;
         } else {
             return TRUE;
@@ -43,29 +43,55 @@ class Form extends CI_Controller {
     function login() {
         $this->form_validation->set_rules('user_id', 'user_id', 'trim|required|min_length[10]|max_length[12]|xss_clean');
         $this->form_validation->set_rules('passwd', 'passwd', 'trim|required');
-       
+
         $iphone = $this->input->post('user_id');
         $pwd = $this->input->post('passwd');
-      
+
         $pwd = do_hash($pwd, 'md5');
-        echo $pwd;
-      
-        $query = $this->db->get_where('users', array('user_id' => $iphone,'passwd' => $pwd)); 
+
+
+        $query = $this->db->get_where('users', array('user_id' => $iphone, 'passwd' => $pwd));
         //相当于 select * from users where user_id = $iphone and passwd= $pwd ;
-        if ($this->form_validation->run() == FALSE  || $query->num_rows() < 1) {
+        $arr_session = $this->session->all_userdata();
+        foreach ($arr_session as $key => $value) {
+            echo $key . "=>" . $value;
+            echo br();
+        }
+        if ($this->form_validation->run() == FALSE || $query->num_rows() < 1) {
             $this->load->view('pages/login');
         } else {
-            $this->load->view('home');
+            $newdata = array(
+                'username' => $iphone,
+                'logged_in' => TRUE
+            );
+            $this->session->set_userdata($newdata); //添加用户名到session中
+            $this->load->view('/log/include_logout');
         }
+    }
+
+    function logout() {
+        $this->session->sess_destroy(); //销毁session
+        $this->load->view('/log/include_logout');
     }
 
     function login_verify() {
         echo 'success!';
     }
 
+    function myself() {
+
+        $this->load->view('/log/myself');
+        echo '你是：' . $this->session->userdata('username');
+        echo br();
+    }
+
     function home() {
         $this->load->view('home');
-
+        $arr_session = $this->session->all_userdata();
+        foreach ($arr_session as $key => $value) {
+            echo $key . "=>" . $value;
+            echo br();
+        }
     }
 
     function about() {
