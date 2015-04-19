73b76a8c8381f11985bd353d56cadf237ad20328
waten <waten1992@gmail.com>
2015-04-19 20:24:05 +0800

    更改为中文标签提示

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -3,22 +3,22 @@ $config = array(
                  'signup' => array(
                                     array(
                                             'field' => 'user_id',
-                                            'label' => 'phone',
+                                            'label' => '手机号码',
                                             'rules' => 'callback_user_id_check|numeric|trim|required|min_length[10]|max_length[12]|xss_clean|is_unique[users.user_id]'
                                          ),
                                     array(
                                             'field' => 'passwd',
-                                            'label' => 'Password',
+                                            'label' => '密码',
                                             'rules' => 'trim|required|matches[passconf]'
                                          ),
                                     array(
                                             'field' => 'passconf',
-                                            'label' => 'PasswordConfirmation',
+                                            'label' => '密码确认',
                                             'rules' => 'trim|required'
                                          ),
                                     array(
                                             'field' => 'address',
-                                            'label' => 'address',
+                                            'label' => '地址',
                                             'rules' => 'trim|required'
                                          )
                                     )
